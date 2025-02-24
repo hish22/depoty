5f129ba8e37b58a150b003bf82ee715404bef34d
Hisham Aljoghaiman <hish522@hotmail.com>
2025-02-24 19:33:40 +0300

    :rotating_light: test the find packge process. ::heavy_check_mark:: tests passes.

--- cmd/cli/test/test.go
+++ cmd/cli/test/test.go
@@ -2,7 +2,7 @@ package test
 
 import (
 	"depoty/cmd/cli/core"
-	"depoty/internal/updation"
+	"depoty/internal/finding"
 	"fmt"
 
 	"github.com/spf13/cobra"
@@ -13,7 +13,7 @@ func TestCommand() {
 		Use:   "test",
 		Short: "testing",
 		Run: func(cmd *cobra.Command, args []string) {
-			updation.UpdatePkg("slack") // -> Tested , and it is working well !
+			finding.FindPkg("slack") // -> Tested , and it is working well !
 			fmt.Println("This command is just for testing purposes.")
 		},
 	}
