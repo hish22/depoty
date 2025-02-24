ebc36e9851e8b22a333f4d6b0905bf39e5d4fb0e
Hisham Aljoghaiman <hish522@hotmail.com>
2025-02-24 20:02:12 +0300

    :hammer: chore: delete unnecessary parameter.

--- cmd/tui/perform_searching_operation.go
+++ cmd/tui/perform_searching_operation.go
@@ -5,7 +5,7 @@ import (
 	"github.com/rivo/tview"
 )
 
-func performSearchingOperation(app *tview.Application, installPkg *tview.InputField, foundPkgsTable *tview.Table, textPkg *tview.TextView) {
+func performSearchingOperation(installPkg *tview.InputField, foundPkgsTable *tview.Table, textPkg *tview.TextView) {
 	// Start searching by pressing Enter
 	installPkg.SetDoneFunc(func(key tcell.Key) {
 
