8d50c28d277a9fa0928cfa887173013352ec4397
Hisham Aljoghaiman <hish522@hotmail.com>
2025-02-24 19:35:59 +0300

    :rotating_light: test the list packages process. ::heavy_check_mark:: tests passes.

--- cmd/cli/test/test.go
+++ cmd/cli/test/test.go
@@ -2,7 +2,7 @@ package test
 
 import (
 	"depoty/cmd/cli/core"
-	"depoty/internal/finding"
+	"depoty/internal/listing"
 	"fmt"
 
 	"github.com/spf13/cobra"
@@ -13,7 +13,7 @@ func TestCommand() {
 		Use:   "test",
 		Short: "testing",
 		Run: func(cmd *cobra.Command, args []string) {
-			finding.FindPkgInfo("slack") // -> Tested , and it is working well !
+			listing.ListPkgs() // -> Tested , and it is working well !
 			fmt.Println("This command is just for testing purposes.")
 		},
 	}
