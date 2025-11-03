02a94d77101e2c4ff87a1459c68bae40da746004
Hisham Aljoghaiman <hish522@hotmail.com>
2025-11-03 21:30:45 +0300

    refactor: search if a substring is witihn a string instead of equality.

--- cmd/tui/textsearch/search_in_installed_pkgs.go
+++ cmd/tui/textsearch/search_in_installed_pkgs.go
@@ -1,6 +1,10 @@
 package textsearch
 
-import "github.com/rivo/tview"
+import (
+	"strings"
+
+	"github.com/rivo/tview"
+)
 
 func SearchInInstalledPkgs(installPkg *tview.InputField, textPkgs *tview.TextView, listOfPkgs map[string]string, packageTable *tview.Table) {
 	// Search in installed packages
@@ -8,12 +12,22 @@ func SearchInInstalledPkgs(installPkg *tview.InputField, textPkgs *tview.TextVie
 		textPkgs.SetText("typing...")
 		// var pkgs *map[string]string
 		// pkgs = &listOfPkgs
-		for pkg, totalPkg := range listOfPkgs {
-			if text == pkg {
-				textPkgs.SetText(pkg)
-				packageTable.Clear()
-				packageTable.SetCell(0, 0, tview.NewTableCell(totalPkg))
-				textPkgs.SetText(pkg + " found")
+		i := 0
+		packageTable.Clear()
+
+		if len(text) != 0 {
+			for pkg, totalPkg := range listOfPkgs {
+				if strings.Contains(strings.ToLower(totalPkg), strings.ToLower(text)) {
+					textPkgs.SetText(pkg)
+					packageTable.SetCell(i, 0, tview.NewTableCell(totalPkg))
+					i++
+					textPkgs.SetText(pkg + " found")
+				}
+			}
+		} else {
+			for _, totalPkg := range listOfPkgs {
+				packageTable.SetCell(i, 0, tview.NewTableCell(totalPkg))
+				i++
 			}
 		}
 
